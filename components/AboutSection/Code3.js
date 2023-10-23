d4e3cb70fbf3ad7f5208ba69e3b7e00eeb4da195
Nitin Singh Negi <nitinsinghnegi86@gmail.com>
2023-10-23 17:55:05 +0530

    initial commmit

--- components/AboutSection/Code3.js
+++ components/AboutSection/Code3.js
@@ -8,7 +8,7 @@ export default function Code3({line,code,code1,codeColor,code1Color,code2,code2C
             <div className={`col-md-10 col-10  ${about.bd}`}>
                 <div className={`ps-4`}>
                     <span className={`${about.codePadding}`}  style={{letterSpacing:'0.3rem',color:'#3b3553'}}>{padding}</span>
-                    <span style={{color:codeColor}} className={`${about.code} `}>{code}</span>
+                    <span style={{color:codeColor}} className={`${about.code}`}>{code}</span>
                     <span style={{color:code1Color}} className={`${about.code} ps-2`}>'{code1}'</span>
                     <span style={{color:code2Color}} className={`${about.code} ps-2`}>{code2}</span>
                     <span style={{color:code3Color}} className={`${about.code} ps-2`}>'{code3}'</span>
